67c951ec949ea83ef02cb48a64ed98fb2c95aa1d
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-21 17:12:00 +0700

    submit exercise 3

---
+++ bai3.py
@@ -0,0 +1,23 @@
+# Bài 3: Tính S(n) = x + x^2 + x^3 + … + x^n
+
+def sum_exponential(n: int, x: int) -> int:
+    if n == 0:
+        return 1
+
+    cache = x ** 0
+    result = 0
+    for i in range(1, n + 1):
+        cache = cache * x
+        result += cache
+    return result
+
+def sum_exponential_o1(n: int, x: int) -> int:
+    if n == 0:
+        return 1
+
+    return int(x * (x ** n - 1) / (x - 1))
+
+input_n = 4
+input_x = 2
+print(sum_exponential(input_n, input_x))
+print(sum_exponential_o1(input_n, input_x))
