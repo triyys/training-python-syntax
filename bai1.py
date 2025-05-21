c7e2c8094ced28a6b5add96fe6541dbad90de617
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-21 16:46:24 +0700

    chore: bai1

--- bai1.py
+++ bai1.py
@@ -3,7 +3,7 @@
 # O(n)
 def sum_n(n: int) -> int:
     result = 0
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         result += i
     return result
 
@@ -11,6 +11,6 @@ def sum_n(n: int) -> int:
 def sum_1(n: int) -> int:
     return int(n * (n + 1) / 2)
 
-input_n = 2
+input_n = 4
 print(sum_n(input_n))
 print(sum_1(input_n))
