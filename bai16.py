aab0d48dd86244c39778fbf855c32a5127c58dcc
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-23 10:50:17 +0700

    submit exercise 16

---
+++ bai16.py
@@ -0,0 +1,13 @@
+# Bài 16: Tìm chữ số nhỏ nhất của số nguyên dương n
+
+def min_digit(n: int) -> int:
+    result = 10
+
+    while n > 0:
+        n, remainder = divmod(n, 10)
+        result = min(result, remainder)
+
+    return result
+
+input_n = 6988885
+print(min_digit(input_n))
