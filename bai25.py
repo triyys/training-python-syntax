e57ffac422ac004e1ff66d4e7c0b2925565b0a9c
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-23 17:22:39 +0700

    submit exercise 25

---
+++ bai25.py
@@ -0,0 +1,23 @@
+# Bài 25: Viết chương trình nhập vào 1 ngày (ngày, tháng, năm). Tìm ngày trước ngày vừa nhập
+from typing import Tuple
+import calendar
+
+def previous_date(day: int, month: int, year: int) -> Tuple[int, int, int]:
+    if day - 1 == 0:
+        if month - 1 == 0:
+            return 31, 12, year - 1
+        else:
+            return calendar.monthrange(year, month - 1)[1], month - 1, year
+
+    return day - 1, month, year
+
+input_day = 2
+input_month = 1
+input_year = 2021
+print(previous_date(input_day, input_month, input_year))
+
+# Extra test case
+result = (23, 5, 2025)
+for i in range(100):
+    result = previous_date(result[0], result[1], result[2])
+    print(result)
