73c0a47dd8c6766c154b1cf48580740555a0dc88
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-23 11:27:34 +0700

    submit exercise 17

---
+++ bai17.py
@@ -0,0 +1,11 @@
+# Bài 17: Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không
+
+def is_all_odd_digits(n: int) -> bool:
+    while n > 0:
+        if n % 2 == 0:
+            return False
+        n //= 10
+    return True
+
+input_n = 3933392
+print(is_all_odd_digits(input_n))
