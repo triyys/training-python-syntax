8033e55ded8e42b868453cd798c4b479c6ae6b72
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-23 10:47:37 +0700

    refactor: exercise 15

--- bai15.py
+++ bai15.py
@@ -2,14 +2,14 @@
 
 def max_digit(n: int) -> int:
     mutable_n = n // 10
-    result = n - mutable_n * 10
+    result = n % 10
 
     while mutable_n > 0:
-        current_digit = mutable_n - mutable_n // 10 * 10
+        current_digit = mutable_n % 10
         result = max(result, current_digit)
         mutable_n = mutable_n // 10
 
     return result
 
-input_n = 169333
+input_n = 168333
 print(max_digit(input_n))
