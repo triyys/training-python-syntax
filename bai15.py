e7686e01f9efef4e10b54b1362a7b62b31705f31
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-23 10:39:23 +0700

    submit exercise 15

---
+++ bai15.py
@@ -0,0 +1,15 @@
+# Bài 15: Tìm chữ số lớn nhất của số nguyên dương n
+
+def max_digit(n: int) -> int:
+    mutable_n = n // 10
+    result = n - mutable_n * 10
+
+    while mutable_n > 0:
+        current_digit = mutable_n - mutable_n // 10 * 10
+        result = max(result, current_digit)
+        mutable_n = mutable_n // 10
+
+    return result
+
+input_n = 169333
+print(max_digit(input_n))
