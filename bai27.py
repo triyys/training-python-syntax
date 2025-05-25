0121cec5f7bc37b3ead6b57454aeafb0160b9c66
Darkhold <triyys@Darkholds-MacBook-Air.local>
2025-05-25 19:51:52 +0700

    submit exercise 27

---
+++ bai27.py
@@ -0,0 +1,14 @@
+# Bài 27: Viết hàm tìm giá trị lớn nhất trong mảng 1 chiều các số thực
+from typing import List
+
+def max_real(l: List[float]) -> float:
+    result = l[0]
+
+    for i in l:
+        if i > result:
+            result = i
+
+    return result
+
+input_l = [5, 24.05, -53.11, 24.06, 24.07]
+print(max_real(input_l))
